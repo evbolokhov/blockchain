23af08899aae43c07137e29188a711bafe0d7b87
evbolohov <evbolohov@yandex.ru>
2022-12-22 21:51:16 +0300

    init

---
+++ .spyder-py3/history.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sun Dec 18 18:22:53 2022)---
+runfile('C:/Users/BEU/.spyder-py3/temp.py', wdir='C:/Users/BEU/.spyder-py3')
+
+## ---(Wed Dec 21 18:10:04 2022)---
+runfile('C:/Users/BEU/.spyder-py3/Create a Blockchain/blockchain.py', wdir='C:/Users/BEU/.spyder-py3/Create a Blockchain')
+
+## ---(Thu Dec 22 14:50:23 2022)---
+runcell(0, 'C:/Users/BEU/.spyder-py3/Create a Blockchain/blockchain.py')
+runfile('C:/Users/BEU/.spyder-py3/Create a Blockchain/blockchain.py', wdir='C:/Users/BEU/.spyder-py3/Create a Blockchain')
+
+## ---(Thu Dec 22 17:16:47 2022)---
+runfile('C:/Users/BEU/Documents/Blockchain A-Z/Module 1 - Create a Blockchain/blockchain.py', wdir='C:/Users/BEU/Documents/Blockchain A-Z/Module 1 - Create a Blockchain')
+
+## ---(Thu Dec 22 17:27:12 2022)---
+runfile('C:/Users/BEU/Documents/Blockchain A-Z/Module 1 - Create a Blockchain/blockchain.py', wdir='C:/Users/BEU/Documents/Blockchain A-Z/Module 1 - Create a Blockchain')
+runcell(0, 'C:/Users/BEU/Documents/Blockchain A-Z/Module 1 - Create a Blockchain/blockchain.py')
+runfile('C:/Users/BEU/Documents/Blockchain A-Z/Module 1 - Create a Blockchain/blockchain.py', wdir='C:/Users/BEU/Documents/Blockchain A-Z/Module 1 - Create a Blockchain')
+!git init
+
+## ---(Thu Dec 22 18:16:47 2022)---
+conda
+
+## ---(Thu Dec 22 19:09:54 2022)---
+!git init
+gitk
+!gitk
+
+## ---(Thu Dec 22 20:29:28 2022)---
+!git remote add origin https://github.com/evbolokhov/sample.git
+!git add blockchain.py
+!git add C:\Users\BEU\Documents\Blockchain A-Z\Module 1 - Create a Blockchain\blockchain.py
+!git add temp.py
+!git add .spyder-py3\temp.py
+!git push https://github.com/evbolokhov/PythonPY200 
+!git push
+!git remote add origin https://github.com/evbolokhov/PythonPY200
+!git remote
+!git remote --help
+!git remote remove origin
+!git remote
+!git remote add origin https://github.com/evbolokhov/PythonPY200
+!git push
+!git add .
+cd PythonPY200\Атрибуты и методы\Домашнее задание\task11_LinkedList_pop
+!cd PythonPY200\Атрибуты и методы\Домашнее задание\task11_LinkedList_pop
+dir
+!dir
+!git add .
+git status
+!git status
+git commit
+!git commit
+!git push
+!git commit
+!git push
+!git -- help
